c07a92afeb7d18b73813a7b8fca678bd4ed565e4
Dan Carroll <dancarroll@gmail.com>
2010-02-21 12:15:54 -0800

    Updated error templates; changed title and description for RSS feed

--- mysite/blog/feeds.py
+++ mysite/blog/feeds.py
@@ -2,9 +2,9 @@ from django.contrib.syndication.feeds import Feed
 from mysite.blog.models import Entry
 
 class LatestEntries(Feed):
-    title = "erunama.com"
-    link = "/blog/"
-    description = "Test RSS feed"
+    title = "Dan Carroll"
+    link = "/"
+    description = "RSS feed for Dan Carroll's blog (erunama.com)"
     
     def items(self):
         return Entry.objects.published()
