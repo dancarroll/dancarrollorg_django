061f37d69e492c020b24ec5a8b32cbc76719d49e
Dan Carroll <dancarroll@gmail.com>
2010-02-20 16:12:47 -0800

    Removed test polls app

--- mysite/local_settings_template.py
+++ mysite/local_settings_template.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.flatpages',
-    'mysite.polls',
     'mysite.blog',
     'tagging',
     'django.contrib.admin',
--- mysite/settings.py
+++ mysite/settings.py
@@ -100,7 +100,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.flatpages',
-    'mysite.polls',
     'mysite.blog',
     'tagging',
     'django.contrib.admin',
