f89233c6418d1c23b0aa2e223b6f9d1f14ecf7e5
Dan Carroll <dancarroll@gmail.com>
2010-03-04 00:59:38 -0800

    Integrate InfinitePaginator from django-pagination, and use it for the Activity page (fixes #8)

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mysite.blog.models import Entry, SharedItem, Activity
+from mysite.blog.paginator import InfinitePaginator
 
 
 def index(request):
@@ -10,8 +12,24 @@ def index(request):
                 context_instance=RequestContext(request))
 
 def activity(request):
+    activity_list = Activity.objects.published().defer("username", "author", "comments", "guid")
+    paginator = InfinitePaginator(activity_list, 25)
+    
+    # Make sure page request is an int.  If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    
+    try:
+        activities = paginator.page(page)
+    except:
+        raise Http404
+        
     return render_to_response('activity.html',
-                {'activities': Activity.objects.published()[:50] },
+                {'activities': activities,
+                 'has_next': activities.has_next(),
+                 'has_previous': activities.has_previous() },
                 context_instance=RequestContext(request))
 
 def shared_items(request):
