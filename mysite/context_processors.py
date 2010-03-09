a116ab410df0a8c41f181ad2dfd1dbdc6f1cfeab
Dan Carroll <dancarroll@gmail.com>
2010-03-09 01:48:38 -0800

    Removed hardcoded Google Analytics code and put it into settings

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -1,4 +1,13 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 
 def CurrentSite(request):
-    return { 'current_site': Site.objects.get_current() }
+    try:
+        google_analytics_code = settings.GOOGLE_ANALYTICS_CODE
+    except AttributeError:
+        google_analytics_code = ''
+
+    return {
+        'current_site': Site.objects.get_current(),
+        'UA_code': google_analytics_code,
+    }
