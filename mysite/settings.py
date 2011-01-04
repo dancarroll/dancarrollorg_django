702ba5f3468466d99916a21c1713f9d72a1e84da
Dan Carroll <dancarroll@gmail.com>
2011-01-03 23:37:24 -0800

    Add support for range of internal IP addresses

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,7 +4,6 @@ import deploy
 
 DEBUG = deploy.DEBUG
 TEMPLATE_DEBUG = DEBUG
-INTERNAL_IPS = ('127.0.0.1',)
 
 ADMINS = (
     ('Dan Carroll', 'admin@dancarroll.org'),
@@ -118,3 +117,13 @@ if DEBUG:
     DEBUG_TOOLBAR_CONFIG = {
         'INTERCEPT_REDIRECTS': False,
     }
+
+    from fnmatch import fnmatch
+    class glob_list(list):
+        def __contains__(self, key):
+            for elt in self:
+                if fnmatch(key, elt): return True
+            return False
+
+    INTERNAL_IPS = glob_list(['127.0.0.1', '192.168.0.*'])
+
