f8d91f9908ae7e60b18371e2ac2526d382dcecb1
Dan Carroll <dancarroll@gmail.com>
2011-01-15 15:22:12 -0800

    Clean up display of activities in admin
    --HG--
    branch : activitysync-separation

--- mysite/activitysync/admin.py
+++ mysite/activitysync/admin.py
@@ -3,9 +3,10 @@ from activitysync.models import Activity
 from django.contrib import admin
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('source', 'title', 'link', 'pub_date', 'published', 'username', 'author', 'comments')
-    list_filter = ['pub_date', 'source', 'username']
+    list_display = ('source', 'title', 'published', 'link', 'pub_date')
+    list_filter = ['pub_date', 'source', 'username', 'published']
     search_fields = ['title', 'comments']
     date_hierarchy = 'pub_date'
 
 admin.site.register(Activity, ActivityAdmin)
+
